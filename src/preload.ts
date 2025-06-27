5e8cbc41ae9904d88a7e602f692b196222bf2a62
Carlos L <carlos.developer1983@gmail.com>
2025-06-27 15:45:22 +0200

    Add tabs for endpoints, logs and settings

---
+++ src/preload.ts
@@ -0,0 +1,10 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('api', {
+  selectFile: () => ipcRenderer.invoke('select-file'),
+  startServer: (file: string, port: number) => ipcRenderer.invoke('start-server', file, port),
+  stopServer: () => ipcRenderer.invoke('stop-server'),
+  listRoutes: (file: string) => ipcRenderer.invoke('list-routes', file),
+  getLogs: () => ipcRenderer.invoke('get-logs'),
+  clearLogs: () => ipcRenderer.invoke('clear-logs'),
+});
