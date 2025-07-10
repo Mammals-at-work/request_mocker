4402d6e93945d89c0a121281aa39fc9c0a334fb4
CLC <carlos.developer1983@gmail.com>
2025-07-10 19:21:30 +0200

    refactor: remove sample tests and add Vite configuration for Vue

--- frontend/tests/component/sample.cy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-describe('sample component test', () => {
-  it('mounts App component', () => {
-    cy.visit('index.html');
-    cy.contains('Request Mocker');
-  });
-});
--- frontend/tests/e2e/sample.cy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-describe('sample e2e test', () => {
-  it('loads the page', () => {
-    cy.visit('index.html');
-    cy.contains('Start Server');
-  });
-});
