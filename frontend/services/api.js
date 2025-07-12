dedeefb0bdd2f605dc334a0c776ab267cdfe29bf
CLC <carlos.developer1983@gmail.com>
2025-07-12 15:25:27 +0200

    refactor: remove unused API functions and clean up package.json and tsconfig.json

--- frontend/services/api.js
+++
@@ -1,6 +0,0 @@
-export const selectFile = () => window.api.selectFile();
-export const startServer = (file, port) => window.api.startServer(file, port);
-export const stopServer = () => window.api.stopServer();
-export const listRoutes = file => window.api.listRoutes(file);
-export const getLogs = () => window.api.getLogs();
-export const clearLogs = () => window.api.clearLogs();
